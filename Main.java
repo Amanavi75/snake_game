5d543d9b776756688b9a2dbd4891f71cb9d46df3
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-03-25 12:26:02 +0530

    Add files via upload
    run the main java function to run the game as the snake will eat the food everytime the yummy! will be printed in the terminal window

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Window f1= new Window();
+		
+		
+		f1.setTitle("Snake");
+		f1.setSize(400,400);
+		f1.setVisible(true);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+
+	}
+}
